8c129667d38fe60a4c285226f90d480f40c3aedb
Konstantin <kostyakonov@gmail.com>
2024-12-13 16:25:15 +0300

    LSP

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from orders import Order, DebitPaymentProcessor
+from orders import Order, PaymentProcessor, DebitPaymentProcessor, PaypalPaymentProcessor
 
 order = Order()
 order.add_item('keyboard', 1, 50)
@@ -6,5 +6,14 @@ order.add_item('ssd', 1, 150)
 order.add_item('usb cable', 2, 5)
 
 print(order.total_price())
-pay = DebitPaymentProcessor()
-pay.pay(order, security_code='1234567')
+pay = DebitPaymentProcessor(security_code='1234567')
+pay.pay(order)
+
+pay_by_paypal = PaypalPaymentProcessor(email='something@something.com')
+pay_by_paypal.pay(order)
+
+
+# LSP - можно заменить родительским методом 
+def make_payment(order, payment_method: PaymentProcessor):
+    payment_method.pay(order)
+make_payment(order, PaymentProcessor())
--- orders.py
+++ orders.py
@@ -20,25 +20,34 @@ class Order:
 
 class PaymentProcessor(ABC):
     @abstractmethod
-    def pay(self, order: Order, security_code):
+    def pay(self, order: Order):
         pass
 
 
 class DebitPaymentProcessor(PaymentProcessor):
-    def pay(self, order: Order, security_code):
+    def __init__(self, security_code):
+        self.security_code = security_code
+
+    def pay(self, order: Order):
         print('processing debit payment type')
-        print(f'verifying security code: {security_code}')
+        print(f'verifying security code: {self.security_code}')
         order.status = 'paid'
 
 
 class CreditPaymentProcessor(PaymentProcessor):
-    def pay(self, order: Order, security_code):
+    def __init__(self, security_code):
+        self.security_code = security_code
+
+    def pay(self, order: Order):
         print('processing credit payment type')
-        print(f'verifying security code: {security_code}')
+        print(f'verifying security code: {self.security_code}')
         order.status = 'paid'
 
 class PaypalPaymentProcessor(PaymentProcessor):
-    def pay(self, order: Order, security_code):
+    def __init__(self, email):
+        self.email = email  
+
+    def pay(self, order: Order):
         print('processing paypal payment type')
-        print(f'verifying security code: {security_code}')
+        print(f'verifying email address: {self.email}')
         order.status = 'paid'
